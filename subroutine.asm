f6cb986039889bd3ff1b71dc226061fe52726faa
Rob <69918580+bruhmoent@users.noreply.github.com>
2023-12-06 12:45:31 +0100

    Update subroutine.asm

--- subroutine.asm
+++ subroutine.asm
@@ -1,8 +1,9 @@
 section .data
     num1 db 5
     num2 db 2
+    symbol db '-' 
     result db 0
-    result_str db 10     
+    result_str db 10
     result_end equ $ ; string end
 
 section .text
@@ -10,24 +11,41 @@ section .text
 
 _start:
     mov al, byte [num1]    ; move num1 into al register, small byte size
+    cmp byte [symbol], '+' 
+    jz addition            ; jump to addition if symbol equals '+'
+    
+    cmp byte [symbol], '-' 
+    jz substraction 
+    
 
-    call substr
+addition:
+    call add_operation     
+    jmp display_result
+    
+substraction:
+    call substr     
+    jmp display_result
 
+substr:
+    sub al, byte [num2]    ; subtract num2 from num1 (al contains num1 value)
+    mov [result], al       ; store the result in our variable
+    ret
+
+add_operation:
+    add al, byte [num2]    ; add num2 to num1 (al contains num1 value)
+    mov [result], al       
+    ret
+
+display_result:
     add al, '0'            ; convert the num value into ASCII
-    mov [result_str], al   ; move the char into the string
+    mov [result_str], al   ; move the character into the string
 
     mov ecx, result_str    ; load the address of result_str into ecx
-    mov edx, result_end - result_str  ; length of string
+    mov edx, result_end - result_str  ; length of the string
     mov ebx, 1             ; stdout (1)
-    mov eax, 4             ; for sys_write
+    mov eax, 4             ; sys_write
     int 80h
 
     mov ebx, 0
     mov eax, 1             ; sys_exit
     int 80h
-
-substr:
-    sub al, byte [num2]   ; substract num1 - num2 (al contains num1 value)
-    mov [result], al      ; store result in our variable
-
-    ret
